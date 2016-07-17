99fb86a8892e25c07f46dce3cb3e1234230a0e0c
shinpei futagami <sfutagami@shinpei-no-MacBook-Pro.local>
2016-07-17 22:35:26 +0900

    インストールスクリプトを追加

---
+++ setup/brew.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+brew install zsh
+brew install zsh-completions
+brew install zplug
+
+brew install peco
+brew install tree
+brew install the_silver_searcher
+brew install tmux
+
+brew install ricty
+brew install boost
+brew install vim
+brew install ctags
+
