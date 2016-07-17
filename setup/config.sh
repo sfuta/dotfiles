f061725075f857eaaae5895df31dfbbaf9d6c523
shinpei futagami <sfutagami@shinpei-no-MacBook-Pro.local>
2016-07-17 22:09:31 +0900

    dotfiles設定スクリプト追加

---
+++ setup/config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# dotfiles配下の各ファイルのシンボリックリンクをホームディレクトリに作成
+# 元々あるファイルに関してはバックアップ
+# ~/.dotfiles/bkup[YYmmddHHiiss]を生成
+
+builtin cd `dirname $0` && builtin cd ../home
+bkupdir=$HOME"/.dotfiles/bkup"`date +%Y%m%d%H%M%S`
+mkdir -p ${bkupdir}
+
+__set_dotfiles ()
+{
+  local dotfilename=$1
+  if [ -e ~/${dotfilename} ]; then
+    mv ~/${dotfilename} ${bkupdir}/
+  fi
+  ln -s `pwd`/${dotfilename} ~/
+}
+
+for filename in `ls -a | grep -E "^\.[a-z]+"`; do
+  __set_dotfiles ${filename}
+done
+
